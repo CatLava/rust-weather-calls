608e3dd12ed8d806852be6eceb8df7678c01f1f5
Evan Schoening <evan.schoening@gmail.com>
2024-02-06 16:31:22 -0800

    working full decode with structs and all

--- weather/src/main.rs
+++ weather/src/main.rs
@@ -1,4 +1,5 @@
-use std::error::Error;
+use std::{error::Error, vec};
+use std::collections::HashMap;
 
 use dotenv::dotenv;
 use reqwest;
@@ -25,7 +26,7 @@ async fn main()  {
     println!("{:?}", first_option.lat);
     println!("{:?}", first_option.lon);
 
-    let weather: WeatherResponse  = get_weather_from_lat_long(&first_option.lat, &first_option.lon).await.unwrap();
+    let weather  = get_weather_from_lat_long(&first_option.lat, &first_option.lon).await.unwrap();
     println!("{:?}", weather);
     println!("{:?}", weather.main);
     println!("{:?}", weather.main.temp_min);
@@ -57,7 +58,7 @@ async fn get_lat_long(location: &str) -> Option<Vec<location_map::GeoApiFields>>
         reqwest::StatusCode::OK =>{
             match body.json::<Vec<location_map::GeoApiFields>>().await {
                 Ok(body) => {
-                    println!("Body success {:?}", body);
+                    // println!("Body success {:?}", body);
                     Some(body)
                 }
                 _ => {
@@ -78,34 +79,56 @@ async fn get_lat_long(location: &str) -> Option<Vec<location_map::GeoApiFields>>
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct WeatherResponse {
-    coord: String,
-    weather: String,
+    coord: HashMap<String, f32>,
+    weather: Vec<WeatherBasic>,
     base: String, 
     main: WeatherMain,
-    visibility: String,
-    wind: String,
-    rain: String,
-    clouds: String,
-    dt: String,
-    sys: String,
-    timezone: String,
-    id: String,
+    visibility: i32,
+    wind: WeatherWind,
+    rain: Option<String>,
+    clouds: HashMap<String, i32>,
+    dt: i64, // This is datetime, can convert
+    sys: WeatherSys,
+    timezone: i64,
+    id: i64,
     name: String,
-    code: String
+    code: Option<i32>
 
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct WeatherMain {
-    temp: String,
-    feels_like: String,
-    temp_min: String,
-    temp_max: String,
-    pressure: String,
-    humidity: String
+pub struct WeatherBasic {
+    id: i32,
+    main: String,
+    description: String,
+    icon: String
 }
 
-async fn get_weather_from_lat_long(lat: &str, lon: &str) -> Result<WeatherResponse, reqwest::Error>{
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WeatherMain {
+    temp: f32,
+    feels_like: f32,
+    temp_min: f32,
+    temp_max: f32,
+    pressure: i32,
+    humidity: i32
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WeatherWind {
+    speed: f32,
+    deg: i32,
+    gust: Option<i8>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WeatherSys {
+    r#type: i8,
+    id: i64,
+    country: String,
+    sunrise: i64, // Date string can convert
+    sunset: i64
+}
+async fn get_weather_from_lat_long(lat: &str, lon: &str) -> Option<WeatherResponse> {
     let weather_api_token = std::env::var("WEATHER_API_KEY").expect("GEOCODING_API_KEY must be set.");
     // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
     let mut url = "https://api.openweathermap.org/data/2.5/weather?".to_string();
@@ -116,12 +139,31 @@ async fn get_weather_from_lat_long(lat: &str, lon: &str) -> Result<WeatherRespon
     let body = client.get(url)
         .send()
         .await
-        .unwrap()
-        .text()
-        .await;
+        .unwrap();
+        // .text()
+        // .await;
     println!("Weather");
     println!("{:?}", body);
-    // TODO need to get return type into a struct 
-    return body
+    let resp =  match body.status() {
+        reqwest::StatusCode::OK =>{
+            match body.json::<WeatherResponse>().await {
+                Ok(body) => {
+                    println!("Body success {:?}", body);
+                    Some(body)
+                }
+                _ => {
+                    println!("unable to move forward");
+                    None
+                }
+            }
+        },
+        // TODO need to move this into the success response 
+
+        _ => {
+            panic!("Uh oh! Something unexpected happened.");
+        },
+    };
+    println!("{:?}", resp);
+    return resp
 
 }
